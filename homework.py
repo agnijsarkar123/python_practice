e6b281f9ccd9745208e7dea8ae7f4572a7851bea
agnijsarkar123 <agnijsarkar359@gmail.com>
2024-10-29 12:03:00 +0000

    homework1 done

---
+++ homework.py
@@ -0,0 +1,9 @@
+a = int(input("Enter your percentage: "))
+if (a>90):
+    print("Your grade is A")
+elif (a>80 and a<=90):
+    print("Your grade is B")
+elif (a>=60 and a<=80):
+    print("Your grade  is C")
+elif (a<60):
+    print("Your grade  is D")
