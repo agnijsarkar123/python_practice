b85feccf8c8f29bb32ecea2d693c4b493e3260c4
agnijsarkar123 <agnijsarkar359@gmail.com>
2024-11-07 11:20:43 +0000

    made a ,

--- homework4.py
+++ homework4.py
@@ -1,5 +1,5 @@
 num=int(input("Enter a number: "))
 if num % 2 == 0 and num % 3 == 0:
-    print("{num}is divisible by both 2 and 3.")
+    print(f"{num} is divisible by both 2 and 3.")
 else:
-    print("{num} is not divisible by both 2 and 3.")
+    print(f"{num} is not divisible by both 2 and 3.")
