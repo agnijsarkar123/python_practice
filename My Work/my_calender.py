5254a660f54142541b2ec65d0216606308320dcf
agnijsarkar123 <agnijsarkar359@gmail.com>
2024-12-27 14:38:57 +0000

    My Work

---
+++ My Work/my_calender.py
@@ -0,0 +1,6 @@
+#lets make a calender
+import calendar
+yy = int(input("Enter the year you want in the calender: "))
+mm = int(input("Enter the month of the year you want: "))
+#now lets see how my calender gets printed.
+print(calender.month(yy,mm))
